be641e7db1b6d21d349a425d6a6aaccc182d510c
Olwethu <ongubo21@andover.edu>
2018-07-23 09:07:21 -0400

    my app
    i have designed my app

---
+++ reminders.js
@@ -0,0 +1,20 @@
+
+//    var reminder = prompt("Please add a reminder");
+    //document.getElementById('addreminder').innerHtml=
+  //  if (reminder != null){
+  //    document.getElementById("addreminder").innerHTML =
+    // person;
+
+
+
+//  function updateMyReminder() {
+///
+//  var theOutput = document.getElementById("output");
+  //theOutput.innerHTML = myOutput;}
+  function myFunction() {
+    var task = prompt("Please enter your task");
+    if (task != null) {
+        document.getElementById("demo").innerHTML =
+         task;
+    }
+}
